d8d957837a711c304eb6960cb83daeeee81e63fe
ggrill <guenther.grill@gmail.com>
2024-08-27 14:33:13 -0400

    Streamline legend creation

--- paint2.js
+++ paint2.js
@@ -1,224 +1,136 @@
 var legends2 = {
-    'dis_m3_pyr': [
-        [0, 10, 50, 100, 1000, 10000, 25000, 50000, 230000],
-        ["hsl(207, 88%, 80%)", "hsl(207, 89%, 70%)", "hsl(207, 89%, 60%)", "hsl(207, 89%, 50%)", "hsl(207, 89%, 40%)", "hsl(207, 89%, 30%)", "hsl(207, 89%, 20%)", "hsl(207, 89%, 10%)", "black"],
-        "Natural Discharge (m3)"
+  dis_m3_pyr: [
+    [0, 10, 50, 100, 1000, 10000, 25000, 50000, 230000],
+    [
+      "hsl(207, 88%, 80%)",
+      "hsl(207, 89%, 70%)",
+      "hsl(207, 89%, 60%)",
+      "hsl(207, 89%, 50%)",
+      "hsl(207, 89%, 40%)",
+      "hsl(207, 89%, 30%)",
+      "hsl(207, 89%, 20%)",
+      "hsl(207, 89%, 10%)",
+      "black",
     ],
-    'pac_pc_sse': [
-        [0, 1, 10, 50, 99, 100],
-        ["hsl(0,0%,70%)", "hsl(199,98%,61%)", "hsl(314,73%,24%)", "hsl(5,80%,55%)", "hsl(33,74%,50%)", "black"],
-        "Protected Areas (%)"
+    "Natural Discharge (m3)",
+  ],
+  pac_pc_sse: [
+    [0, 1, 10, 50, 99, 100],
+    [
+      "hsl(0,0%,70%)",
+      "hsl(199,98%,61%)",
+      "hsl(314,73%,24%)",
+      "hsl(5,80%,55%)",
+      "hsl(33,74%,50%)",
+      "black",
     ],
-    'ari_ix_sav': [
-        [0, 1, 50, 100, 150, 200, 227],
-        ["hsl(19,77%,38%)", "hsl(28,65%,54%)", "hsl(118,72%,66%)", "hsl(108,80%,33%)", "hsl(204,72%,41%)", "hsl(220,73%,38%)", "black"],
-        "Global Aridity Index"
+    "Protected Areas (%)",
+  ],
+  ari_ix_sav: [
+    [0, 1, 50, 100, 150, 200, 227],
+    [
+      "hsl(19,77%,38%)",
+      "hsl(28,65%,54%)",
+      "hsl(118,72%,66%)",
+      "hsl(108,80%,33%)",
+      "hsl(204,72%,41%)",
+      "hsl(220,73%,38%)",
+      "black",
     ],
-    'soc_th_sav': [
-        [0, 10, 25, 50, 100, 1041],
-        ["#fddd58", "#f7ba3e", "#d68422", "#9e4410", "#6b0500", "black"],
-        "Organic Carbon Content in Soil"
-    ]
+    "Global Aridity Index",
+  ],
+  soc_th_sav: [
+    [0, 10, 25, 50, 100, 1041],
+    ["#fddd58", "#f7ba3e", "#d68422", "#9e4410", "#6b0500", "black"],
+    "Organic Carbon Content in Soil",
+  ],
 };
 
 var paint = {};
 var paint2 = {};
 
-map_center = [-60, -10]
-startzoom = 2
-flytozoom = 3
-speed = 0.1
+map_center = [-60, -10];
+startzoom = 2;
+flytozoom = 3;
+speed = 0.1;
 
-wrapper_section = 'map-wrapper'
+wrapper_section = "map-wrapper";
 
 var legends = {
-    'dis_m3_pyr': [[0, 10, 50, 100, 1000, 10000, 25000, 50000],
-    ["hsl(207, 88%, 80%)",
-        "hsl(207, 89%, 70%)",
-        "hsl(207, 89%, 60%)",
-        "hsl(207, 89%, 50%)",
-        "hsl(207, 89%, 40%)",
-        "hsl(207, 89%, 30%)",
-        "hsl(207, 89%, 20%)",
-        "hsl(207, 89%, 10%)",],
-        "Natural Discharge (m3)"],
-    'pac_pc_sse': [[0, 1, 10, 50, 99, 100],
-    ["hsl(0,0%,70%)",
-        "hsl(199,98%,61%)",
-        "hsl(314,73%,24%)",
-        "hsl(5,80%,55%)",
-        "hsl(33,74%,50%)"
-    ],
-        "Protected Areas (%)"],
-    'ari_ix_sav': [[0, 1, 50, 100, 150, 200],
+  dis_m3_pyr: [
+    [0, 10, 50, 100, 1000, 10000, 50000, 230000],
     [
-        "hsl(19,77%,38%)",
-        "hsl(28,65%,54%)",
-        "hsl(118,72%,66%)",
-        "hsl(108,80%,33%)",
-        "hsl(204,72%,41%)",
-        "hsl(220,73%,38%)"
+      "hsl(207, 88%, 80%)",
+      "hsl(207, 89%, 70%)",
+      "hsl(207, 89%, 60%)",
+      "hsl(207, 89%, 50%)",
+      "hsl(207, 89%, 40%)",
+      "hsl(207, 89%, 30%)",
+      "hsl(207, 89%, 20%)",
+      "hsl(207, 89%, 10%)",
     ],
-        "Global Aridity Index"]
-
+    "Natural Discharge (m3)",
+  ],
+  pac_pc_sse: [
+    [0, 1, 10, 50, 99, 100],
+    [
+      "hsl(0,0%,70%)",
+      "hsl(199,98%,61%)",
+      "hsl(314,73%,24%)",
+      "hsl(5,80%,55%)",
+      "hsl(33,74%,50%)",
+      "yellow",
+    ],
+    "Protected Areas (%)",
+  ],
+  ari_ix_sav: [
+    [0, 1, 50, 100, 150, 200, 227],
+    [
+      "hsl(19,77%,38%)",
+      "hsl(28,65%,54%)",
+      "hsl(118,72%,66%)",
+      "hsl(108,80%,33%)",
+      "hsl(204,72%,41%)",
+      "hsl(220,73%,38%)",
+    ],
+    "Global Aridity Index",
+  ],
+  soc_th_sav: [
+    [0, 10, 25, 50, 100, 1041],
+    ["#fddd58", "#f7ba3e", "#d68422", "#9e4410", "#6b0500", "black"],
+    "Organic Carbon Content in Soil",
+  ],
 };
 
-function update_style(map_name, legends) {
+function update_style_new(map_name, legends) {
+  if (!legends[map_name]) {
+    console.error(`Legend for ${map_name} not found`);
+    return null;
+  }
 
-    paint = {
-        "fill-color": [
-            'case',
-            ["==", map_name, 'dis_m3_pyr'],
-            [
-                "step",
-                ["get", "dis_m3_pyr"],
-                // start with color
-                legends.dis_m3_pyr[1][0],
-                // first breakpoint value = 0
-                legends.dis_m3_pyr[0][0],
-                legends.dis_m3_pyr[1][0],
-                legends.dis_m3_pyr[0][1],
-                legends.dis_m3_pyr[1][1],
-                legends.dis_m3_pyr[0][2],
-                legends.dis_m3_pyr[1][2],
-                legends.dis_m3_pyr[0][3],
-                legends.dis_m3_pyr[1][3],
-                legends.dis_m3_pyr[0][4],
-                legends.dis_m3_pyr[1][4],
-                legends.dis_m3_pyr[0][5],
-                legends.dis_m3_pyr[1][5],
-                legends.dis_m3_pyr[0][6],
-                legends.dis_m3_pyr[1][6],
-                legends.dis_m3_pyr[0][7],
-                legends.dis_m3_pyr[1][7]
+  const legend = legends[map_name];
+  const breakpoints = legend[0];
+  const colors = legend[1];
 
-            ],
-            ["==", map_name, "pac_pc_sse"],
-            [
-                "step",
-                ["get", "pac_pc_sse"],
-                // start with color
-                legends.pac_pc_sse[1][0],
-                // first breakpoint value = 0
-                legends.pac_pc_sse[0][0],
-                legends.pac_pc_sse[1][0],
-                legends.pac_pc_sse[0][1],
-                legends.pac_pc_sse[1][1],
-                legends.pac_pc_sse[0][2],
-                legends.pac_pc_sse[1][2],
-                legends.pac_pc_sse[0][3],
-                legends.pac_pc_sse[1][3],
-                legends.pac_pc_sse[0][4],
-                legends.pac_pc_sse[1][4],
-                legends.pac_pc_sse[0][5],
-                legends.pac_pc_sse[1][5]
+  let caseExpression = [
+    "case",
+    ["==", ["get", map_name], null],
+    "rgba(0, 0, 0, 0)", // transparent for null values
+  ];
 
-            ],
-            ["==", map_name, "ari_ix_sav"],
-            [
-                "step",
-                ["get", "ari_ix_sav"],
-                // start with color
-                legends.ari_ix_sav[1][0],
-                // first breakpoint value = 0
-                legends.ari_ix_sav[0][0],
-                legends.ari_ix_sav[1][0],
-                legends.ari_ix_sav[0][1],
-                legends.ari_ix_sav[1][1],
-                legends.ari_ix_sav[0][2],
-                legends.ari_ix_sav[1][2],
-                legends.ari_ix_sav[0][3],
-                legends.ari_ix_sav[1][3],
-                legends.ari_ix_sav[0][4],
-                legends.ari_ix_sav[1][4],
-                legends.ari_ix_sav[0][5],
-                legends.ari_ix_sav[1][5],
+  // Create step expression
+  let stepExpression = ["step", ["get", map_name], colors[0]];
 
-            ],
-            // default value must be present
-            "black",
-        ],
-        "fill-opacity": 1,
-        "fill-outline-color": "white"
-    };
-    paint2 = {
-        "fill-color": [
-            'case',
-            ["==", map_name, 'dis_m3_pyr'],
-            [
-                "step",
-                ["get", "dis_m3_pyr"],
-                // start with color
-                legends.dis_m3_pyr[1][0],
-                // first breakpoint value = 0
-                legends.dis_m3_pyr[0][0],
-                legends.dis_m3_pyr[1][0],
-                legends.dis_m3_pyr[0][1],
-                legends.dis_m3_pyr[1][1],
-                legends.dis_m3_pyr[0][2],
-                legends.dis_m3_pyr[1][2],
-                legends.dis_m3_pyr[0][3],
-                legends.dis_m3_pyr[1][3],
-                legends.dis_m3_pyr[0][4],
-                legends.dis_m3_pyr[1][4],
-                legends.dis_m3_pyr[0][5],
-                legends.dis_m3_pyr[1][5],
-                legends.dis_m3_pyr[0][6],
-                legends.dis_m3_pyr[1][6],
-                legends.dis_m3_pyr[0][7],
-                legends.dis_m3_pyr[1][7]
+  for (let i = 0; i < breakpoints.length - 1; i++) {
+    stepExpression.push(breakpoints[i]);
+    stepExpression.push(colors[i]);
+  }
 
-            ],
-            ["==", map_name, "pac_pc_sse"],
-            [
-                "step",
-                ["get", "pac_pc_sse"],
-                // start with color
-                legends.pac_pc_sse[1][0],
-                // first breakpoint value = 0
-                legends.pac_pc_sse[0][0],
-                legends.pac_pc_sse[1][0],
-                legends.pac_pc_sse[0][1],
-                legends.pac_pc_sse[1][1],
-                legends.pac_pc_sse[0][2],
-                legends.pac_pc_sse[1][2],
-                legends.pac_pc_sse[0][3],
-                legends.pac_pc_sse[1][3],
-                legends.pac_pc_sse[0][4],
-                legends.pac_pc_sse[1][4],
-                legends.pac_pc_sse[0][5],
-                legends.pac_pc_sse[1][5]
+  // Add step expression to case expression
+  caseExpression.push(stepExpression);
 
-            ],
-            ["==", map_name, "ari_ix_sav"],
-            [
-                "step",
-                ["get", "ari_ix_sav"],
-                // start with color
-                legends.ari_ix_sav[1][0],
-                // first breakpoint value = 0
-                legends.ari_ix_sav[0][0],
-                legends.ari_ix_sav[1][0],
-                legends.ari_ix_sav[0][1],
-                legends.ari_ix_sav[1][1],
-                legends.ari_ix_sav[0][2],
-                legends.ari_ix_sav[1][2],
-                legends.ari_ix_sav[0][3],
-                legends.ari_ix_sav[1][3],
-                legends.ari_ix_sav[0][4],
-                legends.ari_ix_sav[1][4],
-                legends.ari_ix_sav[0][5],
-                legends.ari_ix_sav[1][5],
-
-            ],
-            // default value must be present
-            "hsl(157, 89%, 40%)",
-        ],
-        "fill-opacity": 1,
-        "fill-outline-color": "white"
-    };
-
-    console.log(paint, paint2);
-
-    return paint, paint2;
+  return {
+    "fill-color": caseExpression,
+    "fill-opacity": 1,
+  };
 }
